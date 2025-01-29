22f01d6e14b725fe78bb48a8b1b759170c24127b
yologger <yologger1013@gmail.com>
2025-01-29 20:47:40 +0900

    chore: init react project

---
+++ webpack.development.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+require('dotenv').config({ path: './.env.development' });
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development', // none, development, production
+  devtool: 'source-map', // https://webpack.js.org/configuration/devtool/#devtool
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    plugins: [new TsconfigPathsPlugin()]
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'), // 번들링 결과물 경로
+    filename: 'main.js' // 번들링 결과물 파일명,
+  },
+  optimization: { minimizer: [] },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'ts-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(jpeg|jpg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new MiniCssExtractPlugin({ filename: 'app.css' }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser'
+    }),
+    new webpack.EnvironmentPlugin({
+      ...process.env
+    })
+  ],
+  devServer: {
+    port: 3000,
+    historyApiFallback: true
+  }
+};
